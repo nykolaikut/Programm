d6f02a33797ade7716e86ef61664f802d5b92e04
Nykolai Kuteinykov <nykolai.kuteinykov@i.ua>
2018-04-17 14:07:44 +0300

    commit from work to github

--- src/com/kuteinykov/programm/App.java
+++ src/com/kuteinykov/programm/App.java
@@ -10,7 +10,6 @@ public class App {
     public static void main(String[] args) throws IOException {
 
         CmdLineService cmd = new CmdLineServiceImpl(new ContactServiceImpl());
-        
         cmd.runMenu();
 
     }
